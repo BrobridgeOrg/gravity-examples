b08d9a339f660c5e1d928f440faef953943351b5
Kenny Chen <kenny@brobridge.com>
2021-06-20 02:18:16 +0800

    fix typo: catergory to category

--- deployments/demo2/createTable.sql
+++ deployments/demo2/createTable.sql
@@ -1,13 +1,13 @@
 create table products(
   name CHAR(80) primary key,
-  catergory CHAR(80),
+  category CHAR(80),
   warehouse CHAR(80),
   quantity INT
 );
 
 create table orders(
   name CHAR(80) primary key,
-  catergory CHAR(80),
+  category CHAR(80),
   warehouse CHAR(80),
   quantity INT
 );
