9908ca6bd3b43e558c8dc0ece867a621e2b56a09
Kenny Chen <kenny@brobridge.com>
2021-06-19 21:54:39 +0800

    add demo2 for quick-start pipeline customization

---
+++ deployments/demo2/createTable.sql
@@ -0,0 +1,13 @@
+create table products(
+  name CHAR(80) primary key,
+  catergory CHAR(80),
+  warehouse CHAR(80),
+  quantity INT
+);
+
+create table orders(
+  name CHAR(80) primary key,
+  catergory CHAR(80),
+  warehouse CHAR(80),
+  quantity INT
+);
